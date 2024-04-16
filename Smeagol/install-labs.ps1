39d1fe26a983d3dd649700cdfd13ecdb8bc3511c
The Dumb <info@hemisphera.com>
2024-04-16 13:28:40 +0200

    Create separate scripts for "tools-internal" and "tools-labs"

--- Smeagol/install-labs.ps1
+++ Smeagol/install-labs.ps1
@@ -1,4 +1,4 @@
 $ErrorActionPreference = "Stop"
 Import-Module BcContainerHelper
 Invoke-Expression ". { $(Invoke-RestMethod https://raw.githubusercontent.com/EOS-Solutions/Scripts/master/Smeagol/Install-RemoteServiceInSession.ps1 -Headers @{"Cache-Control" = "no-cache"}) }"
-Install-RemoteServiceInSession -SourceFeed "tools-internal"
+Install-RemoteServiceInSession -SourceFeed "tools-labs"
