10dea588ffe2d825d85d898e34261f25f965fa51
The Dumb <info@hemisphera.com>
2024-10-09 08:26:20 +0200

    Add script to install gordon service via gin.exe

---
+++ Gordon/InstallGordonService.ps1
@@ -0,0 +1,10 @@
+param(
+    [Parameter(Mandatory = $true)]
+    [string] $ContainerName
+)
+
+Write-Host "Loading gin"
+$GinScriptUri = "https://raw.githubusercontent.com/EOS-Solutions/Scripts/master/Installer/download-gin.ps1"
+Invoke-Expression ". { $(Invoke-RestMethod $GinScriptUri -Headers @{"Cache-Control" = "no-cache"}) }"
+
+gin.exe install bare.webapi --scope $ContainerName
