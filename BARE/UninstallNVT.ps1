a938c715a870464490bca9946a180c02500497c6
The Dumb <info@hemisphera.com>
2025-12-01 14:16:34 +0100

    Remove obsolete scripts

--- BARE/UninstallNVT.ps1
+++
@@ -1,4 +0,0 @@
-$ErrorActionPreference = "Stop"
-Import-Module BcContainerHelper
-Invoke-Expression ". { $(Invoke-RestMethod https://raw.githubusercontent.com/EOS-Solutions/Scripts/master/BARE/Uninstall-NVTInSession.ps1 -Headers @{"Cache-Control" = "no-cache"}) }"
-Uninstall-NVTInSession
--- BARE/install.ps1
+++
@@ -1,4 +0,0 @@
-$ErrorActionPreference = "Stop"
-Import-Module BcContainerHelper
-Invoke-Expression ". { $(Invoke-RestMethod https://raw.githubusercontent.com/EOS-Solutions/Scripts/master/BARE/Install-RemoteServiceInSession.ps1 -Headers @{"Cache-Control" = "no-cache"}) }"
-Install-RemoteServiceInSession -SourceFeed "tools-internal"
